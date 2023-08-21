b12ed7ff9dfb850f1e9467e2c2b5d31363c9e45b
Soumen Jana <soumyaroy13336@gmail.com>
2023-08-21 22:53:12 +0530

    node practice

---
+++ server.js
@@ -0,0 +1,21 @@
+// const http = require('http');
+// const server = http.createServer();
+// console.log("Hello World")
+// server.listen(3000);
+
+// const http = require('http');
+// function handleRequest(request, responce) {
+//     responce.statusCode = 200;
+//     responce.end('<h1>hello World ! </h1>');
+// }
+// const server = http.createServer(handleRequest);
+// server.listen(8080);
+
+// const http = require('http');
+// function handleRequest(request, responce) {
+//     if (request.url === '/currenttime') {
+//         Response.statusCode = 200;
+//     Response.end('<h1'+ new Date().toISOString() + '<h1');
+//    } else if (request.url === '/') {
+//     responce.statusCode = 200;
+//    }
